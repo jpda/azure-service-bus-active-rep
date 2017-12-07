5ccfb329f80c52485d6b4d2ffe2ce454ecb9273e
jpda <john.dandison@live.com>
2017-12-07 15:53:05 -0500

    Add project files.

---
+++ azure-service-bus-active-sender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_service_bus_active_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
